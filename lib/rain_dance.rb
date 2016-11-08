39be3137bd96c2864284572ceaad9fef5e6271f5
Krishnan Mani <km@krishnanm.com>
2016-11-08 12:18:23 +0530

    Now processes stacks

--- lib/rain_dance.rb
+++ lib/rain_dance.rb
@@ -1,16 +1,32 @@
 require 'yaml'
 
-# require_relative 'independent_stack'
-# require_relative 'context_stack'
+require_relative 'independent_stack'
 
 class RainDance
 
   def initialize(artifacts_path)
     @path = artifacts_path
+    load_manifest
   end
 
-  def list
-    YAML.load_file(File.join(@path, 'manifest.yml'))
+  def do_jig!
+    stacks = []
+    manifest["templates"].each do |template|
+      stacks.push(IndependentStack.new(File.join(@path, templates_path, template)))
+    end
+    stacks.each { |stack| stack.process! }
+  end
+
+  def load_manifest
+    @manifest = YAML.load_file(File.join(@path, 'manifest.yml'))
+  end
+
+  def manifest
+    @manifest.dup
+  end
+
+  def templates_path
+    manifest["path"]
   end
 
 end
--- spec/rain_dance_spec.rb
+++ spec/rain_dance_spec.rb
@@ -1,12 +1,41 @@
+require 'aws-sdk'
+
 require_relative '../lib/rain_dance'
 
+
+artifacts_path = File.join(File.dirname(__FILE__), 'dance')
+dance = RainDance.new(artifacts_path)
+
 RSpec.describe RainDance do
-  artifacts_path = File.join(File.dirname(__FILE__), 'dance')
 
   it 'reads manifest files available' do
-    dance = RainDance.new(artifacts_path)
-    manifest = dance.list
-    expect(manifest["templates"]).to eql(['abc', 'def'])
+    manifest = dance.manifest
+    expect(manifest["templates"]).to eql(['abc'])
+  end
+
+  it 'reads the path to stacks' do
+    expect(dance.templates_path).to eql('templates')
+  end
+
+end
+
+RSpec.describe RainDance do
+
+  client = Aws::CloudFormation::Client.new(region: 'ap-south-1')
+
+  before(:each) do
+    delete_stack('abc', client)
+  end
+
+  after(:each) do
+    delete_stack('abc', client)
+  end
+
+  it 'processes a template listed in the manifest' do
+    dance.do_jig!
+    stack_resource = Aws::CloudFormation::Resource.new(client: client)
+    created_stack = stack_resource.stack('abc')
+    expect(created_stack.stack_status).to match /CREATE/
   end
 
 end
