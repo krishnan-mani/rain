49811597571505ffb8ed8a0b49ae38c05d1d7abd
Krishnan Mani <km@krishnanm.com>
2016-11-18 11:45:34 +0530

    Removed inaccurate file path assertion

--- rain.rb
+++ rain.rb
@@ -35,9 +35,6 @@ parser.parse!
 manifest_file_name = options[:manifest_file]
 artifacts_path = options[:artifacts_path]
 
-manifest_path = manifest_file_name.nil? ? File.join(artifacts_path, 'manifest.yml') : File.join(artifacts_path, manifest_file_name)
-p manifest_path
-
 app_options = {s3Location: options[:s3_bucket], s3Region: options[:s3_region]}
 
 require_relative 'lib/rain_dance'
